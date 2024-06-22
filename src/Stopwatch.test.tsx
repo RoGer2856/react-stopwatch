048e27e4af1cd14006ef75b2ebac84358866754a
Gergo Roth <roth@bytifex.com>
2024-06-22 15:07:45 +0200

    feat: testing Stopwatch component functionality

--- src/Stopwatch.test.tsx
+++ src/Stopwatch.test.tsx
@@ -1,9 +1,95 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Stopwatch from './Stopwatch';
+import { act } from 'react';
 
-test('renders stopwatch', () => {
-  const {rerender} = render(<Stopwatch />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+function getElapsedTimeText() {
+  return screen.getByLabelText('time elapsed').textContent;
+}
+
+function queryStartButton() {
+  return screen.queryByLabelText('start stopwatch');
+}
+
+function queryPauseButton() {
+  return screen.queryByLabelText('pause stopwatch');
+}
+
+function queryResetButton() {
+  return screen.getByLabelText('reset stopwatch');
+}
+
+test('intial state render; buttons and elapsed time', () => {
+  render(<Stopwatch />);
+
+  expect(getElapsedTimeText()).toBe('00:00:000');
+
+  const startButton = queryStartButton();
+  expect(startButton).toBeInTheDocument();
+  expect(startButton).not.toBeDisabled();
+
+  expect(queryPauseButton()).toBeNull();
+
+  const resetButton = queryResetButton();
+  expect(resetButton).toBeInTheDocument();
+  expect(resetButton).toBeDisabled();
+});
+
+test('start, pause, reset; checking button availabilities and their functions and time elapsing', () => {
+  jest.useFakeTimers();
+
+  render(<Stopwatch />);
+
+  fireEvent.click(queryStartButton()!);
+
+  act(() => {
+    jest.advanceTimersByTime(1500);
+  });
+
+  {
+    expect(getElapsedTimeText()).toBe('00:01:500');
+
+    const startButton = queryStartButton();
+    expect(startButton).toBeNull();
+
+    const pauseButton = queryPauseButton();
+    expect(pauseButton).toBeInTheDocument();
+    expect(pauseButton).not.toBeDisabled();
+
+    const resetButton = queryResetButton();
+    expect(resetButton).toBeInTheDocument();
+    expect(resetButton).toBeDisabled();
+  }
+
+  fireEvent.click(queryPauseButton()!);
+
+  {
+    expect(getElapsedTimeText()).toBe('00:01:500');
+
+    const startButton = queryStartButton();
+    expect(startButton).toBeInTheDocument();
+    expect(startButton).not.toBeDisabled();
+
+    const pauseButton = queryPauseButton();
+    expect(pauseButton).toBeNull();
+
+    const resetButton = queryResetButton();
+    expect(resetButton).toBeInTheDocument();
+    expect(resetButton).not.toBeDisabled();
+  }
+
+  fireEvent.click(queryResetButton()!);
+
+  {
+    expect(getElapsedTimeText()).toBe('00:00:000');
+
+    const startButton = queryStartButton();
+    expect(startButton).toBeInTheDocument();
+    expect(startButton).not.toBeDisabled();
+
+    expect(queryPauseButton()).toBeNull();
+
+    const resetButton = queryResetButton();
+    expect(resetButton).toBeInTheDocument();
+    expect(resetButton).toBeDisabled();
+  }
 });
--- src/Stopwatch.tsx
+++ src/Stopwatch.tsx
@@ -2,7 +2,7 @@ import { ReactElement, useEffect, useState } from 'react';
 import { computeTimeComponents } from './utils';
 import styles from './Stopwatch.module.css';
 
-const TIME_STEP_MILLIS = 100;
+const TIME_STEP_MILLIS = 10;
 
 export default function Stopwatch(): ReactElement {
   const [timeMillis, setTimeMillis] = useState(0);
@@ -33,19 +33,41 @@ export default function Stopwatch(): ReactElement {
   }
 
   const timeComponents = computeTimeComponents(timeMillis);
+  const minutes = String(timeComponents.minutes).padStart(2, '0');
   const seconds = String(timeComponents.secondsInTheMinute).padStart(2, '0');
   const millis = String(timeComponents.millisInTheSecond).padStart(3, '0');
 
   return (
     <>
-      <div>{`${timeComponents.minutes}:${seconds}:${millis}`}</div>
+      <div aria-label="time elapsed">{`${minutes}:${seconds}:${millis}`}</div>
       <div>
         {
           isRunning
-            ? <button className={`${styles.button} ${styles.button__pause}`} onClick={onPause} disabled={!isRunning}>Pause</button>
-            : <button className={`${styles.button} ${styles.button__start}`} onClick={onStart} disabled={isRunning}>Start</button>
+            ? <button
+                className={`${styles.button} ${styles.button__pause}`}
+                onClick={onPause}
+                disabled={!isRunning}
+                aria-label="pause stopwatch"
+              >
+                Pause
+              </button>
+            : <button
+                className={`${styles.button} ${styles.button__start}`}
+                onClick={onStart}
+                disabled={isRunning}
+                aria-label="start stopwatch"
+              >
+                Start
+              </button>
         }
-        <button className={`${styles.button} ${styles.button__reset}`} onClick={onReset} disabled={isRunning || timeMillis === 0}>Reset</button>
+        <button
+          className={`${styles.button} ${styles.button__reset}`}
+          onClick={onReset}
+          disabled={isRunning || timeMillis === 0}
+          aria-label="reset stopwatch"
+        >
+          Reset
+        </button>
       </div>
     </>
   );
